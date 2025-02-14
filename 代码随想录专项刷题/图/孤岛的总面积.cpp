8c2523096e49976856f312ae2eba87b159db955b
koNoJOJOga <1300734270@qq.com>
2025-02-14 11:40:37 +0800

    day13

--- 代码随想录专项刷题/图/孤岛的总面积.cpp
+++ 代码随想录专项刷题/图/孤岛的总面积.cpp
@@ -1,47 +1,48 @@
-#include <iostream>
-#include <vector>
+# include <iostream>
+# include <vector>
 using namespace std;
-int dir[4][2] = {-1, 0, 0, -1, 1, 0, 0, 1};
+
+int dir[4][2] = {-1, 0, 0, -1, 0, 1, 1, 0};
 int cnt;
-void dfs(vector<vector<int>>& grid, int x, int y) {
+void dfs (vector<vector<int>> grid, int x, int y) {
     grid[x][y] = 0;
-    cnt++;
-    for (int i = 0; i < 4; i++) {
-        int nextx = x + dir[i][0];
-        int nexty = y + dir[i][1];
+    ++ cnt;
 
-        if (nextx < 0 || nextx >= grid.size() || nexty < 0 || nexty >= grid[0].size()) continue;
-        if (grid[nextx][nexty] == 0) continue;
+    for (int i = 0; i < 4; ++ i ) {
+        int next_x = x + dir[i][0];
+        int next_y = y + dir[i][1];
 
-        dfs (grid, nextx, nexty);
+        if (next_x < 0 || next_x > grid.size() || next_y < 0 || next_y > grid[0].size())
+            continue;
+        
+        if (grid[next_x][next_y] == 0) continue;
+
+        dfs (grid, next_x, next_y);
     }
-    return;
+
+    return ;
 }
 
-int main() {
+int main () {
     int n, m;
     cin >> n >> m;
     vector<vector<int>> grid(n, vector<int>(m, 0));
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cin >> grid[i][j];
-        }
-    }
 
-    for (int i = 0; i < n; i++) {
+    for(int i = 0; i < n; ++ i )
+        for (int j = 0; j < m; ++j )
+            cin >> grid[i][j];
+
+    // 左右两边
+    for (int i = 0; i < n; ++ i ) {
         if (grid[i][0] == 1) dfs(grid, i, 0);
         if (grid[i][m - 1] == 1) dfs(grid, i, m - 1);
     }
-
-    for (int j = 0; j < m; j++) {
-        if (grid[0][j] == 1) dfs(grid, 0, j);
-        if (grid[n - 1][j] == 1) dfs(grid, n - 1, j);
+    // 上下两边
+    for (int i = 0; i < m; ++ i ) {
+        if (grid[0][i] == 1) dfs(grid, 0, i);
+        if (grid[n - 1][i] == 1) dfs(grid, n - 1, i);
     }
     cnt = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (grid[i][j] == 1) dfs(grid, i, j);
-        }
-    }
-    cout << cnt << endl;
+
+    return 0;
 }
