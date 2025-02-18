0934b7fc683d41ef133d403e2294e5576fa53bce
koNoJOJOga <1300734270@qq.com>
2025-02-18 12:30:10 +0800

    day 15

--- 代码随想录专项刷题/图/冗余连接II.cpp
+++ 代码随想录专项刷题/图/冗余连接II.cpp
@@ -1,7 +1,6 @@
 # include <iostream>
 # include <vector>
 using namespace std;
-
 int n;
 vector<int> father(1001, 0);
 
@@ -15,18 +14,18 @@ int find (int u) {
     return u == father[u] ? u : father[u] = find(father[u]);
 }
 
+void merge (int u, int v) {
+    u = find(u);
+    v = find(v);
+    if (u == v) return;
+    father[v] = u;
+    return;
+}
+
 bool isSame (int u, int v) {
     u = find(u);
     v = find(v);
     return u == v;
-}
-
-void merge (int u, int v) {
-    u = find(u);
-    v = find(v);
-    if (u == v) return ;
-    father[v] = u;
-    return ;
 }
 
 void getRemoveEdge (const vector<vector<int>>& edges) {
@@ -36,17 +35,17 @@ void getRemoveEdge (const vector<vector<int>>& edges) {
             cout << edges[i][0] << " " << edges[i][1];
             return ;
         }
-        else
+        else {
             merge(edges[i][0], edges[i][1]);
+        }
     }
-    return ;
 }
 
-bool isTreeAfterRemoveEdge (const vector<vector<int>>& edges, int deleteEdge) {
+bool isTreeAfterRemoveEdge(const vector<vector<int>>& edges, int deleteEdge) {
     init();
     for (int i = 0; i < n; ++ i ) {
         if (i == deleteEdge) continue;
-        if (isSame(edges[i][0], edges[i][1]))
+        if (isSame(edges[i][0], edges[i][1])) 
             return false;
         merge(edges[i][0], edges[i][1]);
     }
@@ -55,28 +54,27 @@ bool isTreeAfterRemoveEdge (const vector<vector<int>>& edges, int deleteEdge) {
 
 int main () {
     int s, t;
-    cin >> n;
     vector<vector<int>> edges;
+    cin >> n;
     vector<int> inDegree(n + 1, 0);
     for (int i = 0; i < n; ++ i ) {
         cin >> s >> t;
         ++ inDegree[t];
         edges.push_back({s, t});
     }
-    
+
     vector<int> vec;
     for (int i = n - 1; i >= 0; -- i )
         if (inDegree[edges[i][1]] == 2)
             vec.push_back(i);
-    
     if (vec.size() > 0) {
         if (isTreeAfterRemoveEdge(edges, vec[0]))
             cout << edges[vec[0]][0] << " " << edges[vec[0]][1];
-        else
+        else 
             cout << edges[vec[1]][0] << " " << edges[vec[1]][1];
         return 0;
     }
-    
+
     getRemoveEdge(edges);
     return 0;
 }
